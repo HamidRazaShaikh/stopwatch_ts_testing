bc99ad4e2de120ef38451d49317d21cc5fa0bea8
HamidRazaVision <hamidraza.chemistry@gmail.com>
2020-10-16 18:58:07 -0700

    main stopwatch coding completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useRef} from "react";
+import "./index";
 
-function App() {
+const Stopwatch = () => {
+  let [timer, setTimer] = useState<number>(0);
+  let [ isActive , setIsActive] = useState <Boolean>(true);
+  let [ pause , setPause] = useState <Boolean>(true);
+  let [ zero ,  setZero] = useState <Boolean>(true);
+
+  const increment  = useRef<any>(null);
+
+  const start = ()=>{
+    setIsActive(false);
+    setPause(true);
+    setZero(true)  
+
+    increment.current = setInterval( ()=> {
+      setTimer((timer) => timer +1)
+    },1000)
+
+
+  };
+
+  const stop =()=>{
+    setIsActive(true);
+    setPause(false);
+    setZero(true)  
+
+
+    clearInterval(increment.current)
+  };
+
+  const reset = () =>{
+    setIsActive(true);
+    setPause(true);
+    setZero(false)  
+
+    clearInterval(increment.current);
+    setTimer(0)
+
+
+
+  }
+
+  const timeFormate = () => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2);
+    const minutes : any = `${Math.floor( timer/60)}`;
+    const getMinutes : any= `0${ minutes % 60}`.slice(-2);
+    const getHours : any = `0${ Math.floor( timer/3600)}`.slice(-2)
+
+    return  `${getHours}:${getMinutes}:${getSeconds}`
+
+
+  }
+
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="container">
+        <div className="title"> stop watch</div>
+        <div className="display">
+          {timeFormate()}
+         
+        </div>
+        <div className="btn">
+          <button className="btn-start" onClick={start} disabled ={!isActive}>
+            Start
+          </button>
+          <button className="btn-stop" onClick={stop}  disabled ={!pause}>
+            Stop
+          </button>
+          <button  className="btn-reset" onClick = {reset} disabled ={!zero}>Reset</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Stopwatch;
